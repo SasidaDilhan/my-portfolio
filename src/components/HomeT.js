f97651a6e351365eaa5c233c7e4f10292e054d48
IT21158254 <IT21158254@my.sliit.lk>
2023-06-18 20:35:01 +0530

    skills added

---
+++ src/components/HomeT.js
@@ -0,0 +1,46 @@
+import "./HomeT.css";
+import Typewriter from "typewriter-effect";
+
+import React from "react";
+import download from "../assets/download.jpg";
+import { Link } from "react-scroll";
+
+const HomeT = () => {
+  return (
+    <div className="homeT">
+      <div className="mask">
+        <img className="download" src={download} alt="download" />
+      </div>
+      <div className="content">
+        <p className="tt">
+          HI, I'M <span className="sp">Sasida Dilhan</span>
+        </p>
+        <h1>
+          <Typewriter
+            options={{
+              strings: ["Web Developer💻"],
+              autoStart: true,
+              loop: true,
+            }}
+          />
+        </h1>
+        <div>
+          <a className="btn" href="Isuru Sajith-CV.pdf" download="Isuru CV.pdf">
+            RESUME
+          </a>
+          <Link
+            to="form"
+            className="btn-light"
+            smooth={true}
+            duration={500}
+            style={{ cursor: `pointer` }}
+          >
+            Contact
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomeT;
