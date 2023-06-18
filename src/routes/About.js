6b5f6704fd2cd74d628fd7231d21f795ba53e81e
IT21158254 <IT21158254@my.sliit.lk>
2023-06-18 23:58:04 +0530

    done

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import Heroimg2 from "../components/Heroimg2";
+// import Heroimg2 from "../components/Heroimg2";
 import AboutMe from "../components/AboutMe";
 
 const About = () => {
   return (
     <div>
       <Navbar />
-      <Heroimg2 heading="About" text="" />
+      {/* <Heroimg2 heading="About" text="" /> */}
       <AboutMe />
       <Footer />
     </div>
--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import Heroimg2 from "../components/Heroimg2";
+// import Heroimg2 from "../components/Heroimg2";
 import { Form } from "../components/Form";
 
 const Contact = () => {
   return (
     <div>
       <Navbar />
-      <Heroimg2 heading="Contact" text="" />
+      {/* <Heroimg2 heading="Contact" text="" /> */}
       <Form />
       <Footer />
     </div>
