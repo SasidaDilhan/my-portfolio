8ac7aab97564db7045bc383546e888f8aae2a25d
sasida dilhan <it21158254@my.sliit.lk>
2025-06-06 17:45:03 +0530

    backgroun updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,14 +6,12 @@ import MyCV from "../../public/cv/SasidaDilhan.pdf";
 const Hero = () => {
   return (
     <section
-      className="relative h-screen flex items-center justify-center"
+      className="relative h-screen flex items-center justify-center bg-cover bg-center bg-no-repeat"
       style={{
         backgroundImage: `url(${background})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
       }}
     >
-      <div className="absolute inset-0 bg-black bg-opacity-50"></div>
+      <div className="absolute inset-0 bg-black/50"></div>
       <div className="relative z-10 text-center">
         <img
           src={myPhoto}
