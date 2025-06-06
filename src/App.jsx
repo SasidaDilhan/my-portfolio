8eec4864051cad7e40964ca5830e794238dafddb
sasida dilhan <it21158254@my.sliit.lk>
2025-06-06 18:46:11 +0530

    more animations added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import ProjectDetails from "./components/ProjectDetails";
+import AnimatedBackground from "./components/AnimatedBackground";
 
 // import Contact from "./components/Contact";
 import "./App.css";
@@ -9,13 +15,29 @@ import { ContactSection } from "./components/Contact";
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-900 text-white">
-      <Hero />
-      <About />
-      <Skills />
-      <ProjectsSection />
-      <ContactSection />
-    </div>
+    <Router>
+      <div className="relative min-h-screen bg-gray-900 text-white">
+        <AnimatedBackground />
+        <div className="relative z-10">
+          <Navbar />
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <Hero />
+                  <About />
+                  <Skills />
+                  <Projects />
+                  <Contact />
+                </>
+              }
+            />
+            <Route path="/projects/:id" element={<ProjectDetails />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
